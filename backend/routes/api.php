81131d1fc7edc0de0d97480e6b49c780c289240a
Tomás Salto <tomasagustinsalto@gmail.com>
2024-09-24 16:33:52 -0300

    Added routes to edit user data

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use App\Http\Controllers\OptionsController;
+use App\Http\Controllers\PerfilController;
 
 Route::get('/tematicas', [OptionsController::class, 'getTematicas']);
 Route::get('/patologias', [OptionsController::class, 'getPatologias']);
@@ -15,6 +16,11 @@ Route::post('/registrar_paciente', [AuthController::class, 'registerPaciente']);
 
 Route::post('/register_psicologo', [AuthController::class, 'registerPsicologo']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/perfil', [PerfilController::class, 'show']);
+    Route::put('/update-patient', [PerfilController::class, 'update'])->middleware('auth');
+});
 
 Route::middleware([
     'auth:sanctum',
