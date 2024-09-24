0963092e9650f26739723ac423d63b03fdab4250
Tomás Salto <tomasagustinsalto@gmail.com>
2024-09-24 01:51:16 -0300

    Changed logic in Button component to reuse

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -9,6 +9,8 @@ const Button = ({ text, color, disabled, type = "button", onClick }) => {
           ? "!bg-[#C1C1C1] cursor-not-allowed text-white"
           : color === "primary"
           ? "bg-[#75B781] text-white"
+          : color === "third"
+          ? "bg-white border-[2px] border-[#75B781] text-[#75B781] font-bold"
           : "bg-[#2A352F] text-white"
       } px-5 py-3 w-full h-full font-Muli rounded flex items-center text-[16px] justify-center sm:px-10`}
     >
