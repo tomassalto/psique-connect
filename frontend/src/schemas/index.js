2153dcecbaa5fa1619c869bf21d93b4a8351bbf0
Tomás Salto <tomasagustinsalto@gmail.com>
2024-09-23 23:28:29 -0300

    Added loginSchema for validation

--- frontend/src/schemas/index.js
+++ frontend/src/schemas/index.js
@@ -58,3 +58,15 @@ export const registerPsychologistSchema = Yup.object({
     .oneOf([Yup.ref("password"), null], "Las contraseñas deben coincidir")
     .required("Confirmar contraseña es requerido"),
 });
+
+export const loginSchema = Yup.object({
+  email: Yup.string()
+    .required("El email es obligatorio")
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Email inválido"
+    ),
+  password: Yup.string()
+    .min(8, "La contraseña debe tener al menos 8 caracteres")
+    .required("Contraseña es requerida"),
+});
