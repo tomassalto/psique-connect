0a29265cefdb23c871fdbd6ebd25c67708aadb59
Tomás Salto <tomasagustinsalto@gmail.com>
2024-11-22 22:22:14 -0300

    Added loader

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center h-screen">
+      <div className="loader border-t-transparent border-solid border-4 border-greenPsique rounded-full w-8 h-8 animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
