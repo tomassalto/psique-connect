3a89782ef6c35711e0c24af021667af024b48d0e
Tomás Salto <tomasagustinsalto@gmail.com>
2024-11-22 22:22:36 -0300

    Added loader and updated styles

--- frontend/src/components/MatchPsicologos.jsx
+++ frontend/src/components/MatchPsicologos.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import MessageModal from "./MessageModal";
+import Loader from "./Loader";
 
 const MatchPsicologos = () => {
+  const [loading, setLoading] = useState(true);
   const [psicologos, setPsicologos] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedPsicologo, setSelectedPsicologo] = useState(null);
@@ -29,6 +31,8 @@ const MatchPsicologos = () => {
         setPsicologos(data);
       } catch (error) {
         console.error("Error al obtener los psicólogos:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchPsicologos();
@@ -41,52 +45,71 @@ const MatchPsicologos = () => {
 
   return (
     <section className="flex flex-col gap-[30px] justify-center items-center pt-[120px] pb-[70px]">
-      <h1 className="text-3xl font-bold text-center text-[#75B781]">
-        ¡Felicidades! Hiciste match con estos psicólogos:
-      </h1>
-      <div className="flex flex-col gap-[30px] w-[340px] sm:w-[590px] lg:w-[990px] xl:w-[980px] mac:w-[1040px] hd:w-[1036px] fullhd:w-[1120px]">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {psicologos.length > 0
-            ? psicologos.map((psicologo) => (
-                <div
-                  key={psicologo.matricula}
-                  className="p-4 border border-[#75B781] rounded-lg shadow-md hover:shadow-lg transition-shadow"
-                >
-                  <div className="flex flex-col gap-5">
-                    <div>
-                      <p>
-                        <strong>Nombre:</strong> {psicologo.nombre}
-                      </p>
-                      <p>
-                        <strong>Apellido:</strong> {psicologo.apellido}
-                      </p>
-                      <p>
-                        <strong>Email:</strong> {psicologo.email}
-                      </p>
-                      <p>
-                        <strong>Matrícula:</strong> {psicologo.matricula}
-                      </p>
-                      <p>
-                        <strong>Patología:</strong> {psicologo.nombre_patologia}
-                      </p>
-                      <p>
-                        <strong>Corriente:</strong> {psicologo.nombre_corriente}
-                      </p>
-                      <p>
-                        <strong>Temática:</strong> {psicologo.nombre_tematica}
-                      </p>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {" "}
+          <h1 className="text-3xl font-bold text-center text-greenPsique font-Muli">
+            ¡Felicidades! Hiciste match con estos psicólogos:
+          </h1>
+          <div className="flex flex-col gap-[30px] w-[340px] sm:w-[590px] lg:w-[990px] xl:w-[980px] mac:w-[1040px] hd:w-[1036px] fullhd:w-[1120px]">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              {psicologos.length > 0
+                ? psicologos.map((psicologo) => (
+                    <div
+                      key={psicologo.matricula}
+                      className="p-4 border border-greenPsique rounded-lg shadow-md hover:shadow-lg transition-shadow"
+                    >
+                      <div className="flex flex-col gap-5">
+                        <div>
+                          <p>
+                            <strong>Nombre:</strong> {psicologo.nombre}
+                          </p>
+                          <p>
+                            <strong>Apellido:</strong> {psicologo.apellido}
+                          </p>
+                          <p>
+                            <strong>Email:</strong> {psicologo.email}
+                          </p>
+                          <p>
+                            <strong>Matrícula:</strong> {psicologo.matricula}
+                          </p>
+                          <p>
+                            <strong>Patología:</strong>{" "}
+                            {psicologo.nombre_patologia}
+                          </p>
+                          <p>
+                            <strong>Corriente:</strong>{" "}
+                            {psicologo.nombre_corriente}
+                          </p>
+                          <p>
+                            <strong>Temática:</strong>{" "}
+                            {psicologo.nombre_tematica}
+                          </p>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                          <Button
+                            onClick={() =>
+                              (window.location.href = `/calificaciones/${psicologo.matricula}`)
+                            }
+                            color="secondary"
+                            text="Ver Calificaciones"
+                          />
+                          <Button
+                            text="Contactar"
+                            color="primary"
+                            onClick={() => handleContactar(psicologo)}
+                          />
+                        </div>
+                      </div>
                     </div>
-                    <Button
-                      text="Contactar"
-                      color="primary"
-                      onClick={() => handleContactar(psicologo)}
-                    />
-                  </div>
-                </div>
-              ))
-            : null}
-        </div>
-      </div>
+                  ))
+                : null}
+            </div>
+          </div>
+        </>
+      )}
 
       <MessageModal
         showModal={showModal}
