e91c164a08d6cb90bc64d6f7675f8d7e6a03238f
Tomás Salto <tomasagustinsalto@gmail.com>
2024-11-26 14:42:14 -0300

    Added public routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\PacienteController;
 use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\PsychologistController;
 use App\Http\Controllers\MensajeController;
+use App\Http\Controllers\PagoController;
 
 Route::get('/tematicas', [OptionsController::class, 'getTematicas']);
 Route::get('/patologias', [OptionsController::class, 'getPatologias']);
@@ -26,6 +27,8 @@ Route::middleware(['auth:sanctum', 'role:paciente'])->group(function () {
     Route::post('/terminar-relacion', [PacienteController::class, 'terminarRelacion']);
     Route::get('/count-sessions/{matriculaPsicologo}', [PacienteController::class, 'countPastSessions']);
     Route::get('/calificaciones/{matricula}', [PsychologistController::class, 'getPsychologistRatings']);
+    Route::get('/mis-sesiones', [SesionController::class, 'misSesiones']);
+    Route::post('/crear-pago', [PagoController::class, 'crearPago']);
 });
 Route::post('/registrar_paciente', [AuthController::class, 'registerPaciente']);
 Route::post('/register_psicologo', [AuthController::class, 'registerPsicologo']);
@@ -46,4 +49,16 @@ Route::middleware(['auth:sanctum', 'role:psicologo'])->group(function () {
     Route::put('/messages/{id_mensaje}/marcarLeido', [MensajeController::class, 'marcarComoLeido']);
     Route::get('/psychologist/unreadMessages', [MensajeController::class, 'unreadMessagesCount']);
     Route::get('/sesiones-de-hoy', [SesionController::class, 'sesionesDeHoy']);
+    Route::get('/mis-pacientes', [PsychologistController::class, 'getMisPacientes']);
+    Route::get('/sesiones-paciente/{dni_paciente}', [PsychologistController::class, 'getSesionesByPaciente']);
+});
+Route::middleware('auth:sanctum')->group(function () {});
+
+Route::post('/webhook-mp', [PagoController::class, 'webhook']);
+Route::get('/pago-exitoso', [PagoController::class, 'pagoExitoso']);
+Route::get('/pago-fallido', [PagoController::class, 'pagoFallido']);
+Route::get('/pago-pendiente', [PagoController::class, 'pagoPendiente']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/comprobante/{id_sesion}', [PagoController::class, 'getComprobante']);
 });
