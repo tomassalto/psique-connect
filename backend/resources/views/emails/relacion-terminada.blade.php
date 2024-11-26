8f253e7c2e28c71663452e19d433d034fcf4e686
Tomás Salto <tomasagustinsalto@gmail.com>
2024-11-25 22:48:58 -0300

    Added mail body

---
+++ backend/resources/views/emails/relacion-terminada.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>Relación Profesional Finalizada</h1>
+
+    <p>Estimado/a {{ $psicologo->nombre }} {{ $psicologo->apellido }},</p>
+
+    <p>Le informamos que el paciente {{ $paciente->nombre }} {{ $paciente->apellido }}
+        ha decidido finalizar la relación profesional.</p>
+
+    <p>Gracias por sus servicios prestados en Psique-Connect.</p>
+</body>
+
+</html>
