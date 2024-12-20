1ac870a9d8c4c24d6003a2093d1debaf3e0bcc8d
Tomás Salto <tomasagustinsalto@gmail.com>
2024-12-20 15:23:40 -0300

    Docker scripts and config changed

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,22 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
+  adapter: node({ mode: "standalone" }),
+  server: {
+    host: true,
+    port: 4321,
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 1000,
+      },
+    },
+  },
 });
